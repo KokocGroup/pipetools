b994183e6ea61e80000ec0843f71aade6d599ef5
Alexandr Karasev <xsandr.k@gmail.com>
2014-07-24 22:22:19 +0400

    fix

--- pipetools/__init__.py
+++ pipetools/__init__.py
@@ -1,35 +1,28 @@
+from collections import defaultdict
 from functools import wraps
 
 
 def groupby(func):
     @wraps(func)
     def wrapper(iterable):
-        iterable = List(iterable)
-        current_group = []
-        result = []
-        for num, item in enumerate(sorted(iterable)):
-            if num != 0:
-                if item not in current_group:
-                    result.append((current_group[0], func(current_group)))
-                    current_group = [item]
-                    continue
-            current_group.append(item)
-        return List(result)
-
+        result = defaultdict(list)
+        for item in iterable:
+            result[item].append(item)
+        return zip(result.keys(), map(func, result.values()))
     return wrapper
 
 
 def sort(func, **kwargs):
     @wraps(func)
     def wrapper(iterable):
-        return List(sorted(iterable, key=func, reverse=kwargs.get('reverse', False)))
+        return sorted(iterable, key=func, reverse=kwargs.get('reverse', False))
     return wrapper
 
 
 def select(func):
     @wraps(func)
     def wrapper(iterable):
-        return List(filter(func, iterable))
+        return filter(func, iterable)
     return wrapper
 
 
